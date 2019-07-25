e4567cb9b6f30f29d3cad1bb739781309a27f3e6
nishantc7 <35103134+nishantc7@users.noreply.github.com>
2019-07-26 01:15:00 +0530

    Add files via upload

---
+++ euler5.cpp
@@ -0,0 +1,52 @@
+#include<conio.h>
+
+#include<iostream>
+
+using namespace std;
+
+int
+ gcm (long long a, long long b)
+
+{
+  while (a != 0)
+ 
+   {
+     
+ long long c = a;
+
+      a = b % a;
+ 
+     b = c;
+
+    }
+ 
+ return b;
+}
+
+
+int 
+lcm (long long a, long long b)
+
+{
+
+  return a * (b / gcm (a, b));
+
+}
+
+
+int
+main ()
+
+{
+  long long result = 1;
+
+  for (int i = 2; i <= 20; i++)
+  
+  result = lcm (result, i);
+
+ 
+ cout << result;
+  
+return 0;
+
+}
