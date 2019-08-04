1185262d0ae762db08f8dcf783abc866c20dfa54
nishantc7 <35103134+nishantc7@users.noreply.github.com>
2019-08-05 03:10:37 +0530

    Update EULER7.cpp

--- EULER7.cpp
+++ EULER7.cpp
@@ -22,7 +22,7 @@ bool isprime(int n)
 {	
 	if (n % 2 == 0)
 		return false;
-	for (int i =3 ; i < n; i++) 
+	for (int i = 3; i < n; i += 2) 
         if (n % i == 0) 
             return false; 
   
