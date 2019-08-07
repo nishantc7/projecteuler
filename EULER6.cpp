d2f58c61541bef6c1e5fe429b00f54191b1d1bf9
nishantc7 <35103134+nishantc7@users.noreply.github.com>
2019-08-07 21:30:07 +0530

    Update EULER6.cpp

--- EULER6.cpp
+++ EULER6.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 using namespace std;
 int main()
-{	int sum1=0,sum2=0,sum3=0,sum=0;
-	for(int i=1;i<=100;i++)
-	{	
-		sum1=sum1+(i*i);
-		sum2=sum2+i;
-	}
-	sum3=sum2*sum2;
-	sum=(sum3-sum1);
-	cout<<"Difference is"<<sum;
+{	int sum1=0,sum2=0,sum3=0,n=100;
+	sum1=((n*(n+1))*(2*n+1))/6;
+	sum2=((n*(n+1))/2)*((n*(n+1))/2);
+	
+	sum3=(sum2-sum1);
+	cout<<"Difference is"<<sum3;
 }
